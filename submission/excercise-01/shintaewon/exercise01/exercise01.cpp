de81a057d025d63ce99d713c02ef27a5b292aa22
shintaewon <53418280+shintaewon@users.noreply.github.com>
2023-12-14 14:36:00 +0900

    temp commi for making branch

---
+++ submission/excercise-01/shintaewon/exercise01/exercise01.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+	//리스트 선언
+	list<int> num_list;
+
+	int temp;
+
+	//뒤쪽으로 요소 추가하기
+	for (int i = 0; i < 10; i++) {
+		cin >> temp;
+		num_list.push_back(temp);
+	}
+
+
+}
