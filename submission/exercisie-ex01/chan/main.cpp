484e8970cad3f98eed6e75d6629deae7e7613eea
c99seo <88650888+c99seo@users.noreply.github.com>
2023-12-14 15:41:18 +0900

    Update main.cpp

--- submission/exercisie-ex01/chan/main.cpp
+++ submission/exercisie-ex01/chan/main.cpp
@@ -15,24 +15,15 @@ int main()
 	}
 	cin >> a >> b;
 
-	
+
 	list<int>::iterator iter = lst.begin();
-	list<int>::iterator iter2 = lst.begin();
 
-	for (int i = 0; i < a-1; i++) {
-		iter++;
+	lst.remove_if([&](int x) { return x >= a && x < b; });
+
+	// 4. 결과 출력
+	for (int num : lst) {
+		std::cout << num << " ";
 	}
-	for (int i = a; i < b-1; i++) {
-		iter2++;
-	}
-
-	lst.erase(iter, iter2);
-
-	for (list<int>::iterator itera = lst.begin(); itera != lst.end(); itera++) {
-		cout << *itera << endl;
-	}
-	cout << "\n";
-
 
 	return 0;
 }
