27b71250d5682ec6008d679c2d3e3a728a14338b
Nahyun-Kim <50420981+NahyunEE@users.noreply.github.com>
2023-12-14 16:44:33 +0900

    Exercise03

---
+++ submission/exercise-03/NahyunKim/exercise-03/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+template<typename x1,typename x2>
+constexpr x2 gcd(x1 a, x2 b) {
+
+    if (a % b) {
+        return gcd(b, (a % b));
+    }
+    else {
+        return b;
+    }
+
+}
+
+
+int main()
+{
+
+    int a, b;
+    cin >> a;
+    cin >> b;
+    const int num =gcd(a, b);
+
+    cout << num << endl;
+   
+
+}
