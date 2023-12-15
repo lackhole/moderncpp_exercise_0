32a8922db5ef3361399324bd43f0a68e263fd3e7
qkdgmltn <152455860+qkdgmltn@users.noreply.github.com>
2023-12-15 11:40:05 +0900

    Update exercise_08.cpp

--- submission/exercise-08/heesoo/exercise_08.cpp
+++ submission/exercise-08/heesoo/exercise_08.cpp
@@ -1,7 +1,46 @@
-class MyApplication {
+#include <iostream>
+
+
+#ifdef _MSC_VER
+#define MY_FUNCTION__ __FUNCSIG__
+#else
+#define MY_FUNCTION__ __PRETTY_FUNCTION__
+#endif
 
 
 
+
+class MyApplication  {
+public:
+	MyApplication() {
+		std::cout << MY_FUNCTION << '\n';
+	}
+
+	MyApplication(const MyApplication&) {
+		std::cout << MY_FUNCTION << '\n';
+
+	}
+
+	MyApplication(MyApplication&&) noexcept {
+		std::cout << MY_FUNCTION << '\n';
+
+	}
+
+	MyApplication& operator = (const MyApplication&) {
+
+		return *this;
+	}
+
+	MyApplication& operator = (MyApplication&&) noexcept {
+
+		return *this;
+	}
+
+	MyApplication() {
+		std::cout << MY_FUNCTION << '\n';
+
+		//¼Ò¸êÀÚ
+	}
 
 
 };
