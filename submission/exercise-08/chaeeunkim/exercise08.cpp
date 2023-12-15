f529f5dc07469a9db22ef80703defab606998fb8
ChaeYes <80445991+kche1106@users.noreply.github.com>
2023-12-15 11:38:16 +0900

    Update exercise08.cpp

--- submission/exercise-08/chaeeunkim/exercise08.cpp
+++ submission/exercise-08/chaeeunkim/exercise08.cpp
@@ -2,31 +2,42 @@
 #include <iostream>
 using namespace std;
 
+#ifdef _MSC_VER
+#define MY_FUNCTION__ __FUNCSIG__
+#else
+#define MY_FUNCTION __PRETTY_FUNCTION__
+#endif 
+
+
 class MyApplication {
 public:
 	MyApplication() {
-		cout << __FUNCSIG__ << endl;
+		cout << MY_FUNCTION << endl;
 	}
 	MyApplication(const MyApplication& other) {
-		cout << __FUNCSIG__ << endl;
+		cout << MY_FUNCTION << endl;
 	}
 	MyApplication(MyApplication&& other) noexcept {
-		cout << __FUNCSIG__ << endl;
+		cout << MY_FUNCTION << endl;
 	}
 
 	MyApplication& operator=(const MyApplication& other) {
-		cout << __FUNCSIG__ << endl;
+		cout << MY_FUNCTION << endl;
+		return *this;
 	}
 	MyApplication& operator=(MyApplication&& other) noexcept {
-		cout << __FUNCSIG__ << endl;
+		cout << MY_FUNCTION << endl;
+		return *this;
 	}
 
 	~MyApplication() {
-		cout << __FUNCSIG__ << endl;
+		cout << MY_FUNCTION << endl;
 	}
 };
 
 int main() {
 	MyApplication a;
 	MyApplication b = a;
+
+	return 0;
 }
