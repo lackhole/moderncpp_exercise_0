195fbfd43b69f9df2de5ea6f9aaec9259e04a21f
re-ag <763k357@naver.com>
2023-12-15 11:39:41 +0900

    Update exercise-08.cpp

--- submission/exercise-08/jaeeunKim/exercise-08.cpp
+++ submission/exercise-08/jaeeunKim/exercise-08.cpp
@@ -1,5 +1,9 @@
 #include <bits/stdc++.h>
-
+#ifdef _MSC_VER
+#define MY_FUNCTION __FUNCSIG__
+#else
+#define MY_FUNCTION __FUNCSIG__
+#endif
 using namespace std;
 /*
 * exercise-08.cpp
@@ -8,15 +12,27 @@ using namespace std;
 * 2023-12-15
 */
 
+
 class MyApplication {
 public:
-	MyApplication() {	cout << __FUNCSIG__ << '\n'; }
-	MyApplication(const MyApplication& r) { cout << __FUNCSIG__ << '\n'; }
-	MyApplication(MyApplication&& other) noexcept{ cout << __FUNCSIG__ << '\n'; }
-	MyApplication& operator=(const MyApplication &other) { cout << __FUNCSIG__ << '\n'; }
-	MyApplication& operator=(MyApplication&& other) noexcept { cout << __FUNCSIG__ << '\n'; }
-
-	~MyApplication() { cout << __FUNCSIG__ << '\n'; }
+	// default constructor
+	MyApplication() { cout << MY_FUNCTION << '\n'; }
+	// copy constructor
+	MyApplication(const MyApplication& r) { cout << MY_FUNCTION << '\n'; }
+	// move constructor
+	MyApplication(MyApplication&& other) noexcept { cout << MY_FUNCTION << '\n'; }
+	// copy assignment operator
+	MyApplication& operator=(const MyApplication& other)  {
+		cout << MY_FUNCTION << '\n';
+		return *this;
+	}
+	// move assignment operator
+	MyApplication& operator=(MyApplication&& other) noexcept {
+		cout << MY_FUNCTION << '\n';
+		return *this;
+	}
+	//destructor
+	~MyApplication() { cout << MY_FUNCTION << '\n'; }
 };
 
 int main() {
@@ -25,5 +41,9 @@ int main() {
 
 	MyApplication b = a;
 
+	//MyApplication c = std::move(a);
+
+	//MyApplication d;
+	//d = std::move(c);
 	return 0;
 }
