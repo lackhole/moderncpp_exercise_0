ba67d1a61fe1dfe6c8a11df8b0b7e5736a0f6082
re-ag <763k357@naver.com>
2023-12-15 16:01:46 +0900

    Update exercise-11.cpp

--- submission/exercise-11/jaeeunKim/exercise-11.cpp
+++ submission/exercise-11/jaeeunKim/exercise-11.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 /*
-* exercise-09.cpp
+* exercise-11.cpp
 *
 * by jekim
 * 2023-12-15
@@ -10,43 +10,49 @@ using namespace std;
 template<typename T>
 class MyVector {
 public:
+	// default constructor
 	MyVector(const int& size = 3) {
 		data_ = new T[size];
 		capacity_ = size;
 		size_ = 0;
 	}
+
+	// destructor
 	~MyVector() {
-		if(data_ != nullptr)
+		if (data_ != nullptr)
 			delete[] data_;
 	}
 
-	MyVector(const MyVector& r) : capacity_(r.capacity_), size_(r.size_){
+	// copy constructor 
+	MyVector(const MyVector& r) : capacity_(r.capacity_), size_(r.size_) {
 		data_ = new T[capacity_];
 		for (int i = 0; i < size_; ++i) data_[i] = r.data_[i];
 	}
 
+	// move constructor
 	MyVector(MyVector&& r) noexcept : capacity_(r.capacity_), size_(r.size_), data_(r.data_) {
 		r.data_ = nullptr;
 		r.size_ = 0;
 		r.capacity_ = 0;
 	}
-
+	// copy assignment operator
 	MyVector& operator=(const MyVector& r) {
 		if (data_ != nullptr) {
 			delete[] data_;
 			capacity_ = r.capacity_;
 			size_ = r.size_;
 			data_ = new T[capacity_];
-			for (size_t i = 0; i < size_; ++i)
+			for (size_t i = 0; i < size_; ++i) // O(N)
 				data_[i] = r.data_[i];
 		}
 		return *this;
 	}
 
+	// move assignment operator
 	MyVector& operator=(MyVector&& r)noexcept {
 		if (this != &r) {
 			delete[] data_;
-			data_ = r.data_;
+			data_ = r.data_; // O(1)
 			capacity_ = r.capacity_;
 			size_ = r.size_;
 			r.data_ = 0;
@@ -57,7 +63,7 @@ public:
 	}
 	void push_back(const T& val) {
 		//if(capacity_ <= size_) {
-		if(capacity_ == size_){
+		if (capacity_ == size_) {
 			// increase capacity
 			capacity_ *= 2;
 
@@ -77,17 +83,26 @@ public:
 		if (size_ > 0) size_--;
 	}
 
-	T& operator[](size_t i) {
+	T& operator[](size_t i) &{
 		if (i >= 0 && i < size_) return data_[i];
 	}
-	template <typename U = T, typename enable_if<is_rvalue_reference<U&&>::value, int>::type = 0>
-	T&& operator[](size_t i) {
+	
+	const T& operator[](size_t i)const &  {
+		if (i >= 0 && i < size_) return data_[i];
+	}
+
+	T&& operator[](size_t i)&& {
 		if (i >= 0 && i < size_) return move(data_[i]);
 	}
 
-	size_t size() const {
+	const T&& operator[](size_t i)const &&  {
+		if (i >= 0 && i < size_) return move(data_[i]);
+	}
+
+	size_t size(){
 		return size_;
 	}
+
 	void emplace_back(T&& val) {
 		if (size_ == capacity_) {
 			capacity_ *= 2;
@@ -114,7 +129,7 @@ public:
 		data_[size_] = T(forward<Args>(args)...);
 		++size_;
 	}
-//private:
+private:
 	T* data_;
 	size_t size_;
 	size_t capacity_;
@@ -122,52 +137,52 @@ public:
 int main() {
 
 	{  MyVector<int> v1;
-		v1.push_back(1);
-		v1.push_back(2);
-		v1.push_back(3);
-		v1.push_back(4);
-		v1.push_back(5);
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);
+	v1.push_back(4);
+	v1.push_back(5);
 
-		// Copy Constructor
-		MyVector<int> v2 = v1;
-		cout << "Copy Constructor: ";
-		for (size_t i = 0; i < v2.size(); ++i) {
-			cout << v2[i] << " ";
-		}
+	// Copy Constructor
+	MyVector<int> v2 = v1;
+	cout << "Copy Constructor: ";
+	for (size_t i = 0; i < v2.size(); ++i) {
+		cout << v2[i] << " ";
+	}
 
-		// Move Constructor
-		MyVector<int> v3 = move(v1);
-		cout << "\nMove Constructor: ";
-		for (size_t i = 0; i < v3.size(); ++i) {
-			cout << v3[i] << " ";
-		}
+	// Move Constructor
+	MyVector<int> v3 = move(v1);
+	cout << "\nMove Constructor: ";
+	for (size_t i = 0; i < v3.size(); ++i) {
+		cout << v3[i] << " ";
+	}
 
-		// Copy Assignment Operator
-		MyVector<int> v4;
-		v4 = v2;
-		cout << "\nCopy Assignment Operator: ";
-		for (size_t i = 0; i < v4.size(); ++i) {
-			cout << v4[i] << " ";
-		}
+	// Copy Assignment Operator
+	MyVector<int> v4;
+	v4 = v2;
+	cout << "\nCopy Assignment Operator: ";
+	for (size_t i = 0; i < v4.size(); ++i) {
+		cout << v4[i] << " ";
+	}
 
-		// Move Assignment Operator
-		MyVector<int> v5;
-		v5 = move(v2);
-		cout << "\nMove Assignment Operator: ";
-		for (size_t i = 0; i < v5.size(); ++i) {
-			cout << v5[i] << " ";
-		}
+	// Move Assignment Operator
+	MyVector<int> v5;
+	v5 = move(v2);
+	cout << "\nMove Assignment Operator: ";
+	for (size_t i = 0; i < v5.size(); ++i) {
+		cout << v5[i] << " ";
+	}
 
-		cout << "\nPerfecting Forwarding\n";
-		MyVector<string> v;
-		v.push_back("hello");
-		v.push_back("world");
-		const char str[] = "hello again";
+	cout << "\nPerfecting Forwarding\n";
+	MyVector<string> v;
+	v.push_back("hello");
+	v.push_back("world");
+	const char str[] = "hello again";
 
-		v.emplace_back(begin(str), end(str));
-		v.emplace_back(string("hello last time")); 
-		for (size_t i = 0; i < v.size(); ++i)
-			cout << v[i] << ' ';
+	v.emplace_back(begin(str), end(str));
+	v.emplace_back(string("hello last time"));
+	for (size_t i = 0; i < v.size(); ++i)
+		cout << v[i] << ' ';
 	} // v ¼Ò¸ê
 	return 0;
 }
