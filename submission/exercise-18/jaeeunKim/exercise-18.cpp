187d28b99bd5bf38d152a488c8cfd473ecd42004
re-ag <763k357@naver.com>
2023-12-18 16:48:05 +0900

    Create exercise-18.cpp

---
+++ submission/exercise-18/jaeeunKim/exercise-18.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+
+using namespace std;
+/*
+* exercise-18.cpp
+*
+* by jekim
+* 2023-12-18
+*/
+int main() {
+	int x = 0;
+	mutex m;
+	condition_variable cv;
+
+	thread th1([&]() {
+		for (int i = 0; i < 100'000'000; i++) {
+			unique_lock<mutex> lck(m);
+			++x;
+			lck.unlock();
+		}
+		cv.notify_one();
+	});
+	thread th2([&]() {
+		for (int i = 0; i < 100'000'000; i++) {
+			unique_lock<mutex> lck(m);
+			++x;
+			lck.unlock();
+		}
+		cv.notify_one();
+		});
+
+
+	thread th3([&]() {
+		// wait until x becomes 200'000'000
+		unique_lock<mutex> lck(m);
+		cv.wait(lck, [&]() {
+			return x == 200'000'000;
+		});
+		lck.unlock();
+		cout << x << '\n';
+	});
+
+
+	th1.join();
+	th2.join();
+	th3.join();
+	return 0;
+}
