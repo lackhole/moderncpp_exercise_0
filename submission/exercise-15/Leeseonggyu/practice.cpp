66b93abfcb3c752ab14c9ae6562d5b698afda6e2
seonggyu <99774867+handsome-seonggyu@users.noreply.github.com>
2023-12-18 16:11:18 +0900

    Update practice.cpp

--- submission/exercise-15/Leeseonggyu/practice.cpp
+++ submission/exercise-15/Leeseonggyu/practice.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
+#include <chrono>
 
 int x = 0;
 
 int main() {
+	// 예제 #1: 지나간 시간을 체크합니다.
+
+	/*
+	auto now1 = std::chrono::steady_clock::now();
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	auto now2 = std::chrono::steady_clock::now();
+	std::chrono::duration dur = now2 - now1;
+	
+	std::cout << duration_cast<std::chrono::milliseconds>(dur) << std::endl;
+	*/
+	std::mutex m;
 
 	std::thread th1([&]() {
+		std::lock_guard<std::mutex> lck(m);
 		for (int i = 0; i < 100000000; ++i) {
 			x ++;
 		}
