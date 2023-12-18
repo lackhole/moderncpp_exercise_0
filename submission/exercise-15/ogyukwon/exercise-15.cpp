136ac9010fe20c59dd90f15f4c4430e6b16cd8d7
OgyuKwon <koq2541@gmail.com>
2023-12-18 15:35:38 +0900

    Create exercise-15.cpp

---
+++ submission/exercise-15/ogyukwon/exercise-15.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <thread>
+
+int x = 0;
+
+int main() {
+
+
+	std::thread th1 = std::thread([&]() {
+		for (int i = 0; i < 100'000'000; ++i)
+		{
+			x += 1;
+		}
+		});
+
+	std::thread th2 = std::thread([&]() {
+		for (int i = 0; i < 100'000'000; ++i)
+		{
+			x += 1;
+		}
+		});
+
+	if (th1.joinable())
+	{
+		th1.join();
+	}
+
+	if (th2.joinable())
+	{
+		th2.join();
+	}
+	// do something
+
+	std::cout << x << '\n';
+
+	return 0;
+}
