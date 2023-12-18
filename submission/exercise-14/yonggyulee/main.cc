8761c631b7d3f76b55300731a833f35b20a2e0b2
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-18 12:24:24 +0900

    Create main.cc

---
+++ submission/exercise-14/yonggyulee/main.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class MyBaseClass {
+public:
+
+	virtual void Hello() {
+		std::cout << "This is Base\n";
+	}
+
+	virtual ~MyBaseClass() {
+		std::cout << "~MyBaseClass()\n";
+	}
+};
+
+class MyDerivedClass : public MyBaseClass {
+public:
+
+	void Hello() override {
+		std::cout << "This is Derived\n";
+	}
+
+	~MyDerivedClass() {
+		std::cout << "~MyDerivedClass()\n";
+	}
+};
+
+int main() {
+
+	std::unique_ptr<MyBaseClass> ptr
+		= std::make_unique<MyDerivedClass>();
+
+	ptr->Hello();
+
+
+	return 0;
+}
