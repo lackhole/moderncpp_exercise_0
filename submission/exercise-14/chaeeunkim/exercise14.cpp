855900f745f7ad8d68e21bf7ee4fd24458c00a2c
ChaeYes <80445991+kche1106@users.noreply.github.com>
2023-12-18 12:24:29 +0900

    Update exercise14.cpp

--- submission/exercise-14/chaeeunkim/exercise14.cpp
+++ submission/exercise-14/chaeeunkim/exercise14.cpp
@@ -5,18 +5,27 @@ using namespace std;
 class MyBaseClass {
 public:
 	virtual void Hello() {
-		cout << "This is Base" << endl;
+		cout << "This is Base\n";
+	}
+
+	virtual ~MyBaseClass() {
+		cout << "~MyBaseClass()\n";
 	}
 };
 
 class MyDerivedClass : public MyBaseClass {
 public:
-	virtual void Hello() {
-		cout << "This is Derived" << endl;
+	void Hello() override{
+		cout << "This is Derived\n";
+	}
+
+	~MyDerivedClass() {
+		cout << "~MyDerivedClass()\n";
 	}
 };
 
 int main() {
+	//auto ptr = std::make_unique<MyDerivedClass>();
 	std::unique_ptr<MyBaseClass> p = std::make_unique<MyDerivedClass>();
 	p->Hello();
 
